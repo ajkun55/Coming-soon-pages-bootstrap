ec157bfd8dfa3911954d5762c5549ebcdc3e7329
ajkun55 <81357643+ajkun55@users.noreply.github.com>
2024-04-11 18:59:26 +0800

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+/*!
+
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-coming-soon/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
